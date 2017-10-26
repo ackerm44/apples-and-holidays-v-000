92ed0d84bb381aca403427033bd6ec0ba4961419
Amanda Ackerman <ackerm44@gmail.com>
2017-10-26 13:53:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,12 +18,15 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  holiday_supplies[:winter][:christmas] << "Balloons"
+  holiday_supplies[:winter][:new_years] << "Balloons"
 
 end
 
@@ -31,18 +34,17 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_supplies[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-
+  holiday_supplies[season][holiday_name] = supply_array
+  holiday_supplies
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_supplies[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,18 +55,32 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  ######################################
+  season_array = []
+  holiday_array = []
+  holiday_hash.collect do |season, holiday|
+    season_array << season
+    holiday_array << holiday
+  end
+  season_array.each do |season|
+    puts "#{season.to_s.capitalize!}:"
+  end
+  holiday_array.each do |holiday|
+    puts holiday_array
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  collection = []
+  holiday_supplies.each do |season, holiday|
+    holiday.each do |holiday, supplies|
+      supplies.each do |supply|
+          if supply == "BBQ"
+            collection << holiday
+          end
+      end
+    end
+  end
 end
-
-
-
-
-
-
-
